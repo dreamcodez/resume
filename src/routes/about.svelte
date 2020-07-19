c00300b893d31a1ca374293db19c77c9de38af6a
Matthew Elder <sseses@gmail.com>
2020-07-19 16:47:32 -0700

    about and index first pass

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -4,4 +4,11 @@
 
 <h1>About this site</h1>
 
-<p>This is the 'about' page. There's not much here.</p>
+
+<h2>The Stack</h2>
+<ul>
+    <li>This site is built in <a href="https://sapper.svelte.dev">Svelte + Sapper</a></li>
+	<li>The typescript plugin for Svelte is also used.</li>
+	<li>Deployed on Google Cloud Run w/ minimal container. (see github Dockerfile)</li>
+	<li>The code is available on github here.</li>
+</ul>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,5 @@
-<script lang="typescript">
-    export let title: string = "Hello TypeScript";
-    // VS Code will show you an "Unexpected token" error; this is okay at this point
-</script>
-
 <style>
-	h1, figure, p {
+	h1 {
 		text-align: center;
 		margin: 0 auto;
 	}
@@ -16,36 +11,15 @@
 		margin: 0 0 0.5em 0;
 	}
 
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
 	@media (min-width: 480px) {
 		h1 {
-			font-size: 4em;
+			font-size: 3em;
 		}
 	}
 </style>
 
 <svelte:head>
-	<title>{title}</title>
+	<title>Matthew Elders' Home Page</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<h1>Matthew Elders' Home Page</h1>
