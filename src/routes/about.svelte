3a3e18ab6aefce2d11a1714232782bf8b9d1cce4
Matthew Elder <sseses@gmail.com>
2020-07-19 23:13:39 -0700

    update about

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -8,7 +8,7 @@
 <h2>The Stack</h2>
 <ul>
     <li>This site is built in <a href="https://sapper.svelte.dev">Svelte + Sapper</a></li>
-	<li>The typescript plugin for Svelte is also used.</li>
-	<li>Static assets are exported to Google Cloud Storage w/ Cloudflare CDN in front.</li>
-	<li>The code is available on github here.</li>
+	<li>The typescript plugin for Svelte is also used</li>
+	<li>I build a minimalistic Docker container and deploy that to Google Cloud Run</li>
+	<li>The code is available on github <a href="https://github.com/dreamcodez/resume">here</a></li>
 </ul>
