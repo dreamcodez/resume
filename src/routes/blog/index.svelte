b245be84c2a84eafde0679de24b8da91f6be0120
Matthew Elder <sseses@gmail.com>
2020-07-19 21:25:18 -0700

    wip

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,7 +1,7 @@
 <script context="module">
 	export function preload({ params, query }) {
 		return this.fetch(`/blog.json`).then(r => r.json()).then(posts => {
-			return { posts: posts.reverse() };
+			return { posts };
 		});
 	}
 </script>
