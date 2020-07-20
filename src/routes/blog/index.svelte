ae1a5fd55a30621496cc953dd8e749c94f4d25da
Matthew Elder <sseses@gmail.com>
2020-07-19 20:29:14 -0700

    fix blog fetchre

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,6 +1,6 @@
 <script context="module">
 	export function preload({ params, query }) {
-		return this.fetch(`blog.json`).then(r => r.json()).then(posts => {
+		return this.fetch(`/blog.json`).then(r => r.json()).then(posts => {
 			return { posts };
 		});
 	}
