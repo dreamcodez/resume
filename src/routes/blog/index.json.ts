ccce41a70eacdabd436da739dde3b7d14dee6222
Matthew Elder <sseses@gmail.com>
2020-07-19 21:28:40 -0700

    keyed attrs for posts

--- src/routes/blog/index.json.ts
+++ src/routes/blog/index.json.ts
@@ -2,7 +2,7 @@ import posts from './_posts/index.js';
 import { Request } from 'polka';
 import { ServerResponse as Response } from 'http';
 
-const contents = JSON.stringify([...posts].reverse() as Post[]);
+const contents = JSON.stringify(posts as Post[]);
 
 export function get(_req: Request, res: Response) {
 	res.writeHead(200, {
