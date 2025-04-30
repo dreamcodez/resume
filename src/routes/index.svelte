4799c52e431d16d70e1dd928813876b5404632c1
Matthew Elder <sseses@gmail.com>
2025-04-29 23:17:44 -0700

    update frontpage copy

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,12 +2,39 @@
   <title>Matthew Elders' Home Page</title>
 </svelte:head>
 
-<div>
-  <h1>Hire Me (Matthew Elder)</h1>
+<div style="width: 80%; margin: 0 auto">
+  <h2
+    style="font-family:serif;font-style:italic;text-align:left;font-size:1.7em"
+  >
+    With a triumphant beep boop, his status changed:
+  </h2>
+  <code style="display:block; margin: 15px 0">let isHired = true;</code>
+
   <img
-    alt="Jupiter Team Solving Them Problems..."
+    alt="Matthew Elder & Team"
+    style="display:float; float:left; width: 50%; margin: 0 25px 10px 0"
     src="/sophisticated-macman.jpg"
   />
+  <h3>
+    <span style="font-size:2.5em;font-weight:bolder">M</span>atthew Elder
+  </h3>
+  <em>Founder, Technical Architect, Startup Partner</em>
+  <p>
+    Matthew Elder is a seasoned software architect and startup technologist with
+    20+ years of experience designing scalable systems, building full-stack
+    applications, and leading high-performance engineering teams. He has helped
+    launch and scale dozens of products across industries—from fintech and
+    blockchain to media, communications, and SaaS platforms.
+  </p>
+  <p>
+    Matthew’s superpower lies in translating ambitious startup ideas into lean,
+    functional, and future-proof technology. With deep expertise in systems
+    architecture, cloud infrastructure, DevOps, and iterative product
+    development, he partners closely with founders to make smart technical
+    decisions from day one. Whether you’re launching an MVP, replatforming a
+    legacy product, or scaling a critical system under load, Matthew brings
+    tactical clarity and senior-level execution to every engagement.
+  </p>
 </div>
 
 <style>
