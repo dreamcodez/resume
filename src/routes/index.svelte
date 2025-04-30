b79abce159c803116e16c79ddd66b916e442f4ce
Matthew Elder <sseses@gmail.com>
2025-04-29 22:57:11 -0700

    new frontpage image

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,25 +1,31 @@
-<style>
-	h1 {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 3em;
-		}
-	}
-</style>
-
 <svelte:head>
-	<title>Matthew Elders' Home Page</title>
+  <title>Matthew Elders' Home Page</title>
 </svelte:head>
 
-<h1>Matthew Elders' Home Page</h1>
+<div>
+  <h1>Hire Me (Matthew Elder)</h1>
+  <img
+    alt="Jupiter Team Solving Them Problems..."
+    src="/sophisticated-macman.jpg"
+  />
+</div>
+
+<style>
+  h1 {
+    text-align: center;
+    margin: 0 auto;
+  }
+
+  h1 {
+    font-size: 2.8em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 3em;
+    }
+  }
+</style>
